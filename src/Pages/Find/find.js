870c4c760472eca5604da5c3842d35ceae957032
iracoon <podarallarachana@ufl.edu>
2020-02-26 08:34:45 -0800

    GraphQL query

--- src/Pages/Find/find.js
+++ src/Pages/Find/find.js
@@ -2,8 +2,41 @@ import React from "react";
 import Nav from "../../Shared/nav";
 import Context from "../../Store/context";
 
+import ApolloClient, { InMemoryCache, HttpLink } from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
+
 class Find extends React.Component {
   static contextType = Context;
+
+  client = new ApolloClient({
+    uri: `${"https://cors-anywhere.herokuapp.com/"}https://api.yelp.com/v3/graphql`,
+    request: operation => {
+      operation.setContext({
+        headers: {
+          Authorization: process.env.REACT_APP_API_KEY
+        }
+      });
+    }
+  });
+
+  componentDidMount() {
+    this.client
+      .query({
+        query: gql`
+          {
+            business(id: "garaje-san-francisco") {
+              name
+              id
+              alias
+              rating
+              url
+            }
+          }
+        `
+      })
+      .then(result => console.log(result));
+  }
 
   render() {
     return (
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-
 import useGlobalState from "./Store/useGlobalState";
 import Context from "./Store/context";
 
