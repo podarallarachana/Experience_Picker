c3d6467a10127053498b94f837e46443b8a9d3c7
iracoon <podarallarachana@ufl.edu>
2020-02-26 07:50:46 -0800

    Use global state in classes

--- src/Pages/Find/find.js
+++ src/Pages/Find/find.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Nav from "../../Shared/nav";
+import Context from "../../Store/context";
 
 class Find extends React.Component {
+  static contextType = Context;
+
   render() {
     return (
       <div className="App">
         <Nav selected="find" />
+        <h1>{this.context.state.selected}</h1>
       </div>
     );
   }
