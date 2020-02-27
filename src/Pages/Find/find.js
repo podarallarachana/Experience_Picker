53a58e21f02b208bd2dec6432cb7d034b95f80a5
iracoon <podarallarachana@ufl.edu>
2020-02-27 08:52:39 -0800

    Added antd grid to form

--- src/Pages/Find/find.js
+++ src/Pages/Find/find.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Nav from "../../Shared/nav";
+import SearchForm from "./search-form";
 import Context from "../../Store/context";
 import { gql } from "apollo-boost";
 import { graphql } from "react-apollo";
@@ -25,17 +26,6 @@ const getQuery = gql`
 class Find extends React.Component {
   static contextType = Context;
 
-  state = {
-    value: 1
-  };
-
-  onChange = e => {
-    console.log("radio checked", e.target.value);
-    this.setState({
-      value: e.target.value
-    });
-  };
-
   render() {
     return (
       <div>
@@ -47,50 +37,8 @@ class Find extends React.Component {
             : this.props.data.business.id}
         </h2> */}
         <div style={{ padding: "0 50px" }}>
-          <Card>
-            <h4>Category</h4>
-            <Select defaultValue="All..." showSearch style={{ width: "100%" }}>
-              <Option value="All...">All...</Option>
-              <Option value="10">10 mi</Option>
-            </Select>
-            <br />
-            <br />
-            <h4>Subcatergory</h4>
-            <Select defaultValue="All..." showSearch style={{ width: "100%" }}>
-              <Option value="All...">All...</Option>
-              <Option value="10">10 mi</Option>
-            </Select>
-            <br />
-            <br />
-            <h4>Zip code</h4>
-            <Input placeholder="55129" style={{ width: "100%" }} />
-            <br />
-            <br />
-            <h4>Distances</h4>
-            <Slider step={5} max={100} defaultValue={10} />
-            <br />
-            <h4>Rating</h4>
-            <Slider range step={0.5} min={1} max={5} defaultValue={[3, 4.5]} />
-            <br />
-            <h4>Price</h4>
-            <Radio.Group onChange={this.onChange} value={this.state.value}>
-              <Radio value={1}>$</Radio>
-              <Radio value={2}>$$</Radio>
-              <Radio value={3}>$$$</Radio>
-              <Radio value={4}>$$$$</Radio>
-            </Radio.Group>
-            <br />
-            <br />
-            <br />
-            <Button
-              type="primary"
-              shape="round"
-              size={"large"}
-              style={{ width: "200px" }}
-            >
-              Find
-            </Button>
-          </Card>
+          <h1>Find something fun to do!</h1>
+          <SearchForm />
         </div>
       </div>
     );
---
+++ src/Pages/Find/search-form.js
@@ -0,0 +1,77 @@
+import React, { useContext, useEffect } from "react";
+import { Row, Col, Slider, Card, Radio, Select, Input, Button } from "antd";
+
+const { Option } = Select;
+
+const SearchForm = props => {
+  return (
+    <Card>
+      <Row gutter={[16, 16]} type="flex" justify="center">
+        <Col xs={24} sm={24} md={12} lg={10} xl={10}>
+          <h4>Category</h4>
+          <Select defaultValue="All..." showSearch style={{ width: "100%" }}>
+            <Option value="All...">All...</Option>
+            <Option value="10">10 mi</Option>
+          </Select>
+        </Col>
+      </Row>
+      <Row gutter={[16, 16]} type="flex" justify="center">
+        <Col xs={24} sm={24} md={12} lg={10} xl={10}>
+          <h4>Subcatergory</h4>
+          <Select defaultValue="All..." showSearch style={{ width: "100%" }}>
+            <Option value="All...">All...</Option>
+            <Option value="10">10 mi</Option>
+          </Select>
+        </Col>
+      </Row>
+      <Row gutter={[16, 16]} type="flex" justify="center">
+        <Col xs={24} sm={24} md={12} lg={10} xl={10}>
+          <h4>Zip code</h4>
+          <Input placeholder="55129" style={{ width: "100%" }} />
+        </Col>
+      </Row>
+      <Row gutter={[16, 16]} type="flex" justify="center">
+        <Col xs={24} sm={24} md={12} lg={10} xl={10}>
+          <h4>Distances</h4>
+          <Select defaultValue="10" showSearch style={{ width: "100%" }}>
+            <Option value="5">Within 5 mi</Option>
+            <Option value="10">Within 10 mi</Option>
+          </Select>
+        </Col>
+      </Row>
+      <Row gutter={[16, 16]} type="flex" justify="center">
+        <Col xs={24} sm={24} md={12} lg={10} xl={10}>
+          <h4>Rating</h4>
+          <Slider range step={0.5} min={1} max={5} defaultValue={[3, 4.5]} />
+        </Col>
+      </Row>
+      <Row gutter={[16, 16]} type="flex" justify="center">
+        <Col xs={24} sm={24} md={12} lg={10} xl={10}>
+          <h4>Price</h4>
+          <Radio.Group value={2}>
+            <Radio value={1}>$</Radio>
+            <Radio value={2}>$$</Radio>
+            <Radio value={3}>$$$</Radio>
+            <Radio value={4}>$$$$</Radio>
+          </Radio.Group>
+        </Col>
+      </Row>
+      <br />
+      <br />
+      <Row gutter={[16, 16]} type="flex" justify="center">
+        <Col xs={24} sm={24} md={12} lg={10} xl={10}>
+          <Button
+            type="primary"
+            shape="round"
+            size={"large"}
+            style={{ width: "100%" }}
+          >
+            Find
+          </Button>
+        </Col>
+      </Row>
+    </Card>
+  );
+};
+
+export default SearchForm;
