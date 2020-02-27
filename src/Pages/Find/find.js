94e784bd8fe79daaefb054e4df37392d795a298f
iracoon <podarallarachana@ufl.edu>
2020-02-27 08:00:45 -0800

    Added find page form

--- src/Pages/Find/find.js
+++ src/Pages/Find/find.js
@@ -1,9 +1,14 @@
 import React from "react";
 import Nav from "../../Shared/nav";
 import Context from "../../Store/context";
-
 import { gql } from "apollo-boost";
 import { graphql } from "react-apollo";
+import { Layout } from "antd";
+import "./find.scss";
+
+import { Row, Col, Slider, Card, Radio, Select, Input, Button } from "antd";
+const { Option } = Select;
+// const { Content } = Layout;
 
 const getQuery = gql`
   {
@@ -20,24 +25,75 @@ const getQuery = gql`
 class Find extends React.Component {
   static contextType = Context;
 
+  state = {
+    value: 1
+  };
+
+  onChange = e => {
+    console.log("radio checked", e.target.value);
+    this.setState({
+      value: e.target.value
+    });
+  };
+
   render() {
-    if (this.props.data.business) {
-      return (
-        <div className="App">
-          <Nav selected="find" />
-          <h1>{this.context.state.selected}</h1>
-          <h2>{this.props.data.business.id}</h2>
+    return (
+      <div>
+        <Nav selected="find" />
+        {/* <h1>{this.context.state.selected}</h1>
+        <h2>
+          {typeof this.props.data.business === "undefined"
+            ? "NOT"
+            : this.props.data.business.id}
+        </h2> */}
+        <div style={{ padding: "0 50px" }}>
+          <Card>
+            <h4>Category</h4>
+            <Select defaultValue="All..." showSearch style={{ width: "100%" }}>
+              <Option value="All...">All...</Option>
+              <Option value="10">10 mi</Option>
+            </Select>
+            <br />
+            <br />
+            <h4>Subcatergory</h4>
+            <Select defaultValue="All..." showSearch style={{ width: "100%" }}>
+              <Option value="All...">All...</Option>
+              <Option value="10">10 mi</Option>
+            </Select>
+            <br />
+            <br />
+            <h4>Zip code</h4>
+            <Input placeholder="55129" style={{ width: "100%" }} />
+            <br />
+            <br />
+            <h4>Distances</h4>
+            <Slider step={5} max={100} defaultValue={10} />
+            <br />
+            <h4>Rating</h4>
+            <Slider range step={0.5} min={1} max={5} defaultValue={[3, 4.5]} />
+            <br />
+            <h4>Price</h4>
+            <Radio.Group onChange={this.onChange} value={this.state.value}>
+              <Radio value={1}>$</Radio>
+              <Radio value={2}>$$</Radio>
+              <Radio value={3}>$$$</Radio>
+              <Radio value={4}>$$$$</Radio>
+            </Radio.Group>
+            <br />
+            <br />
+            <br />
+            <Button
+              type="primary"
+              shape="round"
+              size={"large"}
+              style={{ width: "200px" }}
+            >
+              Find
+            </Button>
+          </Card>
         </div>
-      );
-    } else {
-      return (
-        <div className="App">
-          <Nav selected="find" />
-          <h1>{this.context.state.selected}</h1>
-          <h2>NO</h2>
-        </div>
-      );
-    }
+      </div>
+    );
   }
 }
 
--- src/Shared/nav.js
+++ src/Shared/nav.js
@@ -1,7 +1,8 @@
 import React, { useContext, useEffect } from "react";
-import { Menu } from "antd";
 import { Link } from "react-router-dom";
 import Context from "../Store/context";
+import { Layout, Menu } from "antd";
+const { Header } = Layout;
 
 const Nav = props => {
   const { state, actions } = useContext(Context);
@@ -16,32 +17,35 @@ const Nav = props => {
 
   return (
     <div>
-      <Menu
-        onClick={e =>
-          actions({
-            type: "setState",
-            payload: { state, selected: e.key }
-          })
-        }
-        selectedKeys={state.selected}
-        mode="horizontal"
-      >
-        <Menu.Item key="home">
-          <Link to="/">Home</Link>
-        </Menu.Item>
-        <Menu.Item key="find">
-          <Link to="/find">Find</Link>
-        </Menu.Item>
-        <Menu.Item key="surpriseme">
-          <Link to="/surpriseme">Surprise Me</Link>
-        </Menu.Item>
-        <Menu.Item key="recommended">
-          <Link to="/recommended">Recommended</Link>
-        </Menu.Item>
-        <Menu.Item key="about">
-          <Link to="/about">About</Link>
-        </Menu.Item>
-      </Menu>
+      <Header style={{ background: "#fff" }}>
+        <Menu
+          onClick={e =>
+            actions({
+              type: "setState",
+              payload: { state, selected: e.key }
+            })
+          }
+          selectedKeys={state.selected}
+          mode="horizontal"
+          theme="light"
+        >
+          <Menu.Item key="home">
+            <Link to="/">Home</Link>
+          </Menu.Item>
+          <Menu.Item key="find">
+            <Link to="/find">Find</Link>
+          </Menu.Item>
+          <Menu.Item key="surpriseme">
+            <Link to="/surpriseme">Surprise Me</Link>
+          </Menu.Item>
+          <Menu.Item key="recommended">
+            <Link to="/recommended">Recommended</Link>
+          </Menu.Item>
+          <Menu.Item key="about">
+            <Link to="/about">About</Link>
+          </Menu.Item>
+        </Menu>
+      </Header>
     </div>
   );
 };
