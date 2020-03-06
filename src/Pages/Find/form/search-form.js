2db0be89e4562c97d53b39276d9faf574fcccd9f
iracoon <podarallarachana@ufl.edu>
2020-03-06 13:43:26 -0600

    Hide more advanced search features

--- src/Pages/Find/form/search-form.js
+++ src/Pages/Find/form/search-form.js
@@ -1,14 +1,30 @@
 import React, { useContext } from "react";
-import { Row, Col, Tabs, Card, Select, Button, Typography } from "antd";
+import {
+  Row,
+  Col,
+  Tabs,
+  Card,
+  Select,
+  Button,
+  Typography,
+  Collapse
+} from "antd";
 import UserLocation from "../../../Shared/user-location";
 import Restaurant from "./restaurant";
 import Recreational from "./recreational";
 import Events from "./events";
 import Context from "../../../Store/context";
 import "../find.scss";
+import {
+  AntDesignOutline,
+  DashboardOutline,
+  CaretRightOutline
+} from "@ant-design/icons";
+import AntdIcon from "@ant-design/icons-react";
 const { Option } = Select;
 const { TabPane } = Tabs;
 const { Title } = Typography;
+const { Panel } = Collapse;
 
 const SearchForm = props => {
   const { state } = useContext(Context);
@@ -19,9 +35,11 @@ const SearchForm = props => {
         <Row gutter={[16, 16]} type="flex" justify="center">
           <Col xs={24} sm={18} md={12} lg={10} xl={10}>
             <Title style={{ color: "#fff", fontWeight: "bold" }}>
-              Find the Perfect Experience!
+              FIND SOMETHING FOR YOU
             </Title>
-            <Card style={{ borderRadius: "0px" }}>
+            <Card
+              style={{ borderRadius: "0px", border: "0px", outline: "0px" }}
+            >
               <Tabs defaultActiveKey="1">
                 <TabPane tab="Restaurants" key="1">
                   <Restaurant curr_form={props.curr_form} />
@@ -34,10 +52,29 @@ const SearchForm = props => {
                 </TabPane>
               </Tabs>
             </Card>
+            <Collapse
+              style={{ borderRadius: "0px", border: "0px", outline: "0px" }}
+              bordered={false}
+              className="site-collapse-custom-collapse"
+            >
+              <Panel
+                header="Get more specific..."
+                key="1"
+                className="site-collapse-custom-panel"
+              >
+                <p>HOJDID</p>
+              </Panel>
+            </Collapse>
             <Button
               type="primary"
               size={"large"}
-              style={{ width: "100%", height: "70px", borderRadius: "0px" }}
+              style={{
+                width: "100%",
+                height: "70px",
+                borderRadius: "0px",
+                outline: "0px",
+                border: "0px"
+              }}
               onClick={e => props.submit_curr_form()}
               disabled={state.val_status === "success" ? false : true}
             >
